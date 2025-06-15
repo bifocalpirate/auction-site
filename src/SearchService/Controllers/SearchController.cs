3dc84d7d1d03df0be5364d13f7b9a53d073c8279
mirrus <sanserif8pt@gmail.com>
2025-06-15 21:59:01 +0200

    WIP: adding the search service

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.RequestHelpers;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
+    {
+        var query = DB.PagedSearch<Item,Item>();        
+
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+        {
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+        }
+        
+        query = searchParams.OrderBy switch{
+            "make" => query.Sort(x=>x.Ascending(a=>a.Make)),
+            "new" => query.Sort(x=>x.Descending(a=>a.CreatedAt)),
+            _ => query.Sort(x=>x.Ascending(a=>a.AuctionEnd))
+        };
+        
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
+
+        var result = await query.ExecuteAsync();
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
+    }
+}
